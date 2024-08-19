00e6f29d32356cf5bf4adf91b6cfdc74ad01c4f3
IgorArh <166037549+IgorArh@users.noreply.github.com>
2024-08-19 10:58:46 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = ('John')
 print  ( 'Name:', name )
 age = 25
 print ( 'Аge: ', age )
-new_age = ( age + 1)
+new_age = int( age + 1)
 print ('New Age:', new_age )
 is_student = True
 print ( 'Is Student: ', is_student)
